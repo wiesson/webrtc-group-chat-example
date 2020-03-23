03521d452b798dd62675d11bdb60796461e31763
Arne Wiese <arne@wiese.me>
2020-03-23 06:56:08 +0100

    Switched to camelCase, added object destructuring

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ io.sockets.on("connection", (socket) => {
   sockets[socket.id] = socket;
 
   console.log("[" + socket.id + "] connection accepted");
-  socket.on("disconnect", function () {
+  socket.on("disconnect", () => {
     for (const channel in socket.channels) {
       part(channel);
     }
@@ -47,8 +47,7 @@ io.sockets.on("connection", (socket) => {
 
   socket.on("join", (config) => {
     console.log("[" + socket.id + "] join ", config);
-    const channel = config.channel;
-    const userdata = config.userdata;
+    const { channel, userData } = config;
 
     if (channel in socket.channels) {
       console.log("[" + socket.id + "] ERROR: already joined ", channel);
@@ -61,10 +60,10 @@ io.sockets.on("connection", (socket) => {
 
     for (const id in channels[channel]) {
       channels[channel][id].emit("addPeer", {
-        peer_id: socket.id,
-        should_create_offer: false,
+        peerId: socket.id,
+        shouldCreateOffer: false,
       });
-      socket.emit("addPeer", { peer_id: id, should_create_offer: true });
+      socket.emit("addPeer", { peerId: id, shouldCreateOffer: true, userData });
     }
 
     channels[channel][socket.id] = socket;
@@ -83,40 +82,38 @@ io.sockets.on("connection", (socket) => {
     delete channels[channel][socket.id];
 
     for (const id in channels[channel]) {
-      channels[channel][id].emit("removePeer", { peer_id: socket.id });
-      socket.emit("removePeer", { peer_id: id });
+      channels[channel][id].emit("removePeer", { peerId: socket.id });
+      socket.emit("removePeer", { peerId: id });
     }
   }
   socket.on("part", part);
 
   socket.on("relayICECandidate", (config) => {
-    const peer_id = config.peer_id;
-    const ice_candidate = config.ice_candidate;
+    const { peerId, iceCandidate } = config;
     console.log(
-      "[" + socket.id + "] relaying ICE candidate to [" + peer_id + "] ",
-      ice_candidate
+      "[" + socket.id + "] relaying ICE candidate to [" + peerId + "] ",
+      iceCandidate
     );
 
-    if (peer_id in sockets) {
-      sockets[peer_id].emit("iceCandidate", {
-        peer_id: socket.id,
-        ice_candidate: ice_candidate,
+    if (peerId in sockets) {
+      sockets[peerId].emit("iceCandidate", {
+        peerId: socket.id,
+        iceCandidate,
       });
     }
   });
 
   socket.on("relaySessionDescription", (config) => {
-    const peer_id = config.peer_id;
-    const session_description = config.session_description;
+    const { peerId, sessionDescription } = config;
     console.log(
-      "[" + socket.id + "] relaying session description to [" + peer_id + "] ",
-      session_description
+      "[" + socket.id + "] relaying session description to [" + peerId + "] ",
+      sessionDescription
     );
 
-    if (peer_id in sockets) {
-      sockets[peer_id].emit("sessionDescription", {
-        peer_id: socket.id,
-        session_description: session_description,
+    if (peerId in sockets) {
+      sockets[peerId].emit("sessionDescription", {
+        peerId: socket.id,
+        sessionDescription,
       });
     }
   });
